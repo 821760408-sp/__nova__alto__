b26d08f5055ed2a3b75908d1c767432aab7f4876
821760408-sp <821760408.sp@gmail.com>
2016-02-13 19:13:18 -0500

    test heroku deploy

--- app.js
+++ app.js
@@ -10,16 +10,10 @@ rtmClient.start();
 //var RTM_EVENTS = slack.RTM_EVENTS;
 rtmClient.on('message', function (message) {
   // Listens to all `message` events from the team
-  
-});
-
-// Send messages
-//var RTM_CLIENT_EVENTS = slack.CLIENT_EVENTS.RTM;
-// you need to wait for the client to fully connect before you can send messages
-rtmClient.on(/*RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED*/'open', function () {
-  // This will send the message 'this is a test message' to the channel identified by id 'C0CHZA86Q'
-  rtmCliet.sendMessage('this is a test message', 'C0CHZA86Q', function messageSent() {
-    // optionally, you can supply a callback to execute once the message has been sent
-  });
+  //noinspection JSUnresolvedFunction
+  var channelGroupOrDM = slack.getChannelGroupOrDMByID(message.channel);
+  if (channelGroupOrDM.is_im) { // https://api.slack.com/types/im
+    rtmClient.sendMessage('test succeeded!', message.channel);
+  }
 });
 
